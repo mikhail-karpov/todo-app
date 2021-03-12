8de458c6f83e006b97393ccdff23c8d8a1ce5e0b
Mikhail <mikhailkarpov@hotmail.com>
2021-03-12 15:35:18 +0200

    Applied styling

--- todo-client/src/main/java/com/mikhailkarpov/todoclient/model/TodoForm.java
+++ todo-client/src/main/java/com/mikhailkarpov/todoclient/model/TodoForm.java
@@ -4,11 +4,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 public class TodoForm {
 
-    @NotBlank(message = "Todo must be provided")
+    @Size(min = 2, max = 255, message = "Description must be at least 2 and at most 255 characters long")
     private String description;
 }
