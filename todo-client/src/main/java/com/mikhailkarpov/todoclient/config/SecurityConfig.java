fa30001b1121dbfb7eab8a714660aa00453a2d36
Mikhail Karpov <mikhailkarpov@hotmail.com>
2021-03-13 15:55:54 +0200

    Minor fixing

--- todo-client/src/main/java/com/mikhailkarpov/todoclient/config/SecurityConfig.java
+++ todo-client/src/main/java/com/mikhailkarpov/todoclient/config/SecurityConfig.java
@@ -5,16 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.function.Consumer;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
