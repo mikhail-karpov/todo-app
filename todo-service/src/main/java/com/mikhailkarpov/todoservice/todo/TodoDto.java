e41c37cb63a3b3aefa68579b0e38e512d36e4d14
Mikhail <mikhailkarpov@hotmail.com>
2021-03-17 15:19:00 +0200

    Tested TodoDto json serialization and deserialization

--- todo-service/src/main/java/com/mikhailkarpov/todoservice/todo/TodoDto.java
+++ todo-service/src/main/java/com/mikhailkarpov/todoservice/todo/TodoDto.java
@@ -1,8 +1,6 @@
 package com.mikhailkarpov.todoservice.todo;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
